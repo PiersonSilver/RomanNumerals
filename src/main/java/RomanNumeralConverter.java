48821975ec3f0ce4d0df7d5f0d171a0ed54d125d
Pierson Silver <silverpierson1@gmail.com>
2024-01-25 14:16:45 -0500

    clean up

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -1,20 +1,10 @@
 import java.util.HashMap;
 
 public class RomanNumeralConverter {
-    /*
-    I = 1
-    V = 5
-    X = 10
-    L = 50
-    C = 100
-    D = 500
-    M = 1000
-     */
     public String romanNumeralConvert(int input){
         if(input > 3999 || input < 1){
             return "input out of range";
         }
-        StringBuilder converted = new StringBuilder();
         HashMap<Integer, String> romanNumerals = new HashMap<>()
         {{
             put(1000, "M");
@@ -34,23 +24,10 @@ public class RomanNumeralConverter {
             symbolCounts.put(romanNumerals.get(v), symbolCounter(input, v));
             input -= v * symbolCounter(input, v);
         }
-        for(int i = values.length-1 ; i >= 0 ; i--){
-            if(symbolCounts.get(romanNumerals.get(values[i])) == 4){
-                if(symbolCounts.get(romanNumerals.get(values[i-1])) == 1){
-                    converted.append(romanNumerals.get(values[i - 2])).append(romanNumerals.get(values[i]));
-                    symbolCounts.replace(romanNumerals.get(values[i-1]), 0);
-                }else{
-                    converted.append(romanNumerals.get(values[i - 1])).append(romanNumerals.get(values[i]));
-                }
-            }else{
-                for(int c = symbolCounts.get(romanNumerals.get(values[i])); c >0; c--){
-                    converted.append(romanNumerals.get(values[i]));
-                }
-            }
-        }
-        converted.reverse();
-        return converted.toString();
+        return convertRN(values, romanNumerals, symbolCounts);
     }
+
+
     private int symbolCounter(int input, int value){
         int count = 0;
         while(input >= value){
@@ -58,5 +35,25 @@ public class RomanNumeralConverter {
             input -= value;
         }
         return count;
+    }
+
+    private String convertRN(int[] values, HashMap<Integer, String> numerals, HashMap<String, Integer> counts){
+        StringBuilder converted = new StringBuilder();
+        for(int i = values.length-1 ; i >= 0 ; i--){
+            if(counts.get(numerals.get(values[i])) == 4){
+                if(counts.get(numerals.get(values[i-1])) == 1){
+                    converted.append(numerals.get(values[i - 2])).append(numerals.get(values[i]));
+                    counts.replace(numerals.get(values[i-1]), 0);
+                }else{
+                    converted.append(numerals.get(values[i - 1])).append(numerals.get(values[i]));
+                }
+            }else{
+                for(int c = counts.get(numerals.get(values[i])); c > 0; c--){
+                    converted.append(numerals.get(values[i]));
+                }
+            }
+        }
+        converted.reverse();
+        return converted.toString();
     }
 }
